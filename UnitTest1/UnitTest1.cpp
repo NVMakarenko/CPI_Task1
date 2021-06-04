3b05cc84d9c543a388c4a607974c95290555e328
NVMakarenko <taradator@ukr.net>
2021-06-04 04:24:42 +0300

    V.1.0.2 TestingTaylorSeriesFunction

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    
+    TEST_CLASS(UnitTest1)
+	{
+        TEST_METHOD(TestMethod1)
+          {
+              float t1 = TaylorFunc(0.0, 5);
+              Assert::AreEqual<float>(0, t1);     
+          };
+	};
+}
